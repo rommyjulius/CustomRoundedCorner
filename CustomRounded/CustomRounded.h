c2d3b3d9f56532f558e1c70d8b1bfad0da497f6c
rommyjulius <62103999+rommyjulius@users.noreply.github.com>
2020-10-20 11:00:01 +0700

    Initial Commit

---
+++ CustomRounded/CustomRounded.h
@@ -0,0 +1,18 @@
+//
+//  CustomRounded.h
+//  CustomRounded
+//
+//  Created by Rommy Julius Dwidharma on 20/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomRounded.
+FOUNDATION_EXPORT double CustomRoundedVersionNumber;
+
+//! Project version string for CustomRounded.
+FOUNDATION_EXPORT const unsigned char CustomRoundedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomRounded/PublicHeader.h>
+
+
